760ec43c314895a44b9c92820598464a3623c19a
Victoria Fawcett <bernard.victoria22@gmail.com>
2025-08-14 19:13:36 -0500

    Upgrade to Tailwind CSS v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
