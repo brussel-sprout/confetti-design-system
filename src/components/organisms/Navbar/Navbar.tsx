93b88e0fc43a316e6dd9720800d5d1794bac86b7
John Fawcett <jrf0110@gmail.com>
2025-08-16 12:14:42 -0500

    Updated Navbar.tsx

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -124,11 +124,6 @@ const NavbarAccountDropdown = React.forwardRef<HTMLDivElement, NavbarAccountDrop
 						'focus:outline-none focus:ring-2 focus:ring-primary/20'
 					)}
 				>
-					<div className="w-8 h-8 bg-primary text-primary-foreground rounded-full flex items-center justify-center">
-						<svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-						</svg>
-					</div>
 					<span>{username}</span>
 					<svg 
 						className={cn(
