76607cc47a6ae21af18f69c81cc6447d0e394d29
John Fawcett <jrf0110@gmail.com>
2025-08-17 13:22:07 -0500

    Updated Navbar.tsx

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -56,7 +56,7 @@ const Navbar = React.forwardRef<HTMLElement, NavbarProps>(
 				)}
 				{...props}
 			>
-				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div className="px-4 sm:px-6 lg:px-8">
 					<div className="flex items-center justify-between h-16">
 						{children}
 					</div>
