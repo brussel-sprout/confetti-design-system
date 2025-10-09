76e6d0fb17a23fd7afa1d1f8cec888ca47408aa3
Victoria <bernard.victoria22@gmail.com>
2025-10-09 16:00:31 -0500

    missed from last commit

---
+++ src/components/molecules/EventBlock/types.ts
@@ -0,0 +1,12 @@
+import type { SuggestionStatusType } from '../../../types/shared'
+
+export interface TimelineEvent {
+  id: string
+  absoluteStart: Date
+  absoluteEnd: Date
+  event_name: string
+  description: string
+  is_suggestion: boolean
+  suggestion_status: SuggestionStatusType
+}
+
