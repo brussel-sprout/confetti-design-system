e9425cf64cee860128f5596f45837d5e5496ecc2
Victoria <bernard.victoria22@gmail.com>
2025-10-09 16:39:15 -0500

    types error

--- src/components/molecules/TimelineAxis/index.ts
+++ src/components/molecules/TimelineAxis/index.ts
@@ -1,5 +1,4 @@
 export { TimelineAxis } from './TimelineAxis'
 export type { TimelineAxisProps } from './TimelineAxis'
-export { TimelineControls } from './TimelineControls.tsx'
-export type { TimelineControlsProps } from './TimelineControls.tsx'
-
+export { TimelineControls } from './TimelineControls'
+export type { TimelineControlsProps } from './TimelineControls'
